3f649ec9d309f652151949827c325706d1a8fdec
Abhinav <abhinavshaji16@gmail.com>
2024-03-30 13:00:51 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// This is comment
+
+// const var let
+
+const acctId = 1
+var acctPass = 1234
+let acctmail = "abc2gmail.com"
+acctstate = "active"  //Should be avoide
+
+// acctId = 2 Constant variable cannot be changed
+
+// var and let are same but var has some issues with block scope always use let
+
+/* 
+This is also Comment
+*/
+
+console.table([acctId, acctPass, acctmail, acctstate
+])
