02e61abb458c1af12f038a85790bd00272fb3b74
Abhinav <abhinavshaji16@gmail.com>
2024-03-30 18:24:08 +0530

    conversion of data types

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,33 @@
+let score = "ABHINAV"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33ABC" => NaN
+// true => 1: false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Abhinav" => true
+
+
+
+let someNumber = true 
+
+let stringNumber = String(someNumber)
+
+console.log(someNumber);
+console.log(stringNumber);
