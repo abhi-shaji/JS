8525e6628d497f9e60c81444ad6da4b2c1e7cf05
Abhinav <abhinavshaji16@gmail.com>
2024-03-30 18:08:33 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treats all code as newer version
+
+
+// number => 2 to the power 0f 53 or 54
+// bigint for use large numbers
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// undefined => no value has been assigned
+// symbol => unique
+// object 
+
+// typeof => used to find the type of data or variable
+
+let a = 10;
+console.log(typeof a)
+console.log(typeof 10)
+console.log(typeof null)
+console.log(typeof undefined)
