a90b3205b55b87a7f1e8ceeadc666aac1008d23a
Abhinav <abhinavshaji16@gmail.com>
2024-03-31 11:47:38 +0530

    operations

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -31,3 +31,28 @@ let stringNumber = String(someNumber)
 
 console.log(someNumber);
 console.log(stringNumber);
+
+// **********************OPERATIONS*************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Abhinav"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1 + 2 +"2");
+
+console.log(true);
+console.log(+true);
+
+let counter = 1 
+++counter
+counter++
+console.log(counter);
