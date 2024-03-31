9f38e739894d8b65bce7f95ed28322ad2dc6e6f5
Abhinav <abhinavshaji16@gmail.com>
2024-03-31 18:06:21 +0530

    Memory Summary(Stack, Heap)

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -7,17 +7,44 @@
 
 // Array, Objects, Functions
 
-const array1 = [1,2,3,4]
+// const array1 = [1,2,3,4]
 
-const product = {
-  name : "Boost",
-  rate : 10 
+// const product = {
+//   name : "Boost",
+//   rate : 10 
+// }
+
+// const myFunction = function() {
+//  console.log(typeof array1);
+//  console.log(typeof product);
+//  console.log(typeof myFunction);
+// }
+
+// myFunction()
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Types of Memory
+//Stack(Primitive), Heap (Non-Primitive)
+
+let name = "Abhinav"
+
+let name2 = name
+
+name2 = "Shaji"
+console.log(name)
+console.log(name2)
+
+
+let userOne = {
+  email:"abc@gmail.com",
+  upi:"user@ybl"
 }
 
-const myFunction = function() {
- console.log(typeof array1);
- console.log(typeof product);
- console.log(typeof myFunction);
-}
+let userTwo = userOne
 
-myFunction()
+userTwo.email = "123@gmail.com"
+
+console.log(userOne)
+console.log(userTwo)
