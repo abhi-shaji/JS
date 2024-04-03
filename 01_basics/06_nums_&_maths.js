27da8d1dec665856632b7a7de2f98aa0e6a50ec8
Abhinav <abhinavshaji16@gmail.com>
2024-04-03 11:53:51 +0530

    numbers

---
+++ 01_basics/06_nums_&_maths.js
@@ -0,0 +1,18 @@
+const score = 400
+
+const balance = new Number(100)
+
+console.log(balance);
+console.log(score);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 23.876
+
+console.log(otherNumber.toPrecision(2));
+
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
