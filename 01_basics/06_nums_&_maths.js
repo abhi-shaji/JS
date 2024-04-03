88dc5b442f582f9740f1fb34f8f74b388a0c34e7
Abhinav <abhinavshaji16@gmail.com>
2024-04-03 12:08:09 +0530

    Math

--- 01_basics/06_nums_&_maths.js
+++ 01_basics/06_nums_&_maths.js
@@ -16,3 +16,24 @@ console.log(otherNumber.toPrecision(2));
 const hundreds = 100000
 console.log(hundreds.toLocaleString());
 console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++++++++++MATSH+++++++++++++++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.657));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.1));
+
+//Random
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+
+console.log((Math.floor(Math.random() * (max - min + 1))) + min)
