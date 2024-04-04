d21ee218dde853c415de7510da11a438bee081f3
Abhinav <abhinavshaji16@gmail.com>
2024-04-04 15:06:37 +0530

    arraybasics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHeros = ["IronMan", "Thor"]
+const hero2 = new Array(4, 6, 8, 10)
+
+console.log(myArr[0]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.includes(0));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice, splice
+
+myArrSlice = myArr.slice(1,3)
+console.log(myArrSlice);
+console.log(myArr);
+
+myArrSplice = myArr.splice(1,3)
+console.log(myArrSplice);
+console.log(myArr);
