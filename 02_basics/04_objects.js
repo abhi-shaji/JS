2bf1872c142883df1d1e95e8338621f8c78111c8
Abhinav <abhinavshaji16@gmail.com>
2024-04-07 15:04:29 +0530

    JSON & destructuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,8 +26,25 @@ const obj2 = {3: "C", 4: "D"}
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+const course = {
+  coursename: "js in hindi",
+  price: "999",
+  courseInstructor: "hitesh"
+}
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+// {
+//   "name": "Abhinav",
+//   "price": "21"
+// }
