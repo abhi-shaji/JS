f277864a288edaaf3398755b25a2a806e9f82728
Abhinav <abhinavshaji16@gmail.com>
2024-04-07 14:45:16 +0530

    object2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+const tinderUser = new Object() 
+
+tinderUser.id = "123abc"
+tinderUser.name = "Abhinav"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularuser = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Abhinav",
+      lastname: "Shaji"
+    }
+  }
+}
+
+// console.log(regularuser.fullname.userfullname.firstname);
+
+const obj1 = {1: "A", 2: "B"}
+const obj2 = {3: "C", 4: "D"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
