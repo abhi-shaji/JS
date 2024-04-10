bd31bb8bc42f5758afe45865337705f12f7ca8c4
Abhinav <abhinavshaji16@gmail.com>
2024-04-10 15:17:20 +0530

    functions1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,27 @@
+function sayMyName() {
+  console.log("Abhinav Shaji");
+  console.log("Abhinav Shaji");
+  console.log("Abhinav Shaji");
+  console.log("Abhinav Shaji");
+}
+
+// sayMyName()
+// sayMyName()
+// sayMyName()
+
+function addTwoNumbers(number1, number2) {
+  // console.log(number1 + number2);
+  let result = number1 + number2
+  return result
+}
+
+const result1 = addTwoNumbers(2, 3)
+
+// console.log(result1);
+
+function userLoginMessage(username) {
+  return `${username} just logged in`
+}
+
+console.log(userLoginMessage("Abhinav"));
+
