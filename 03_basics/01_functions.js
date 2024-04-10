4fdf06e6e78874afaaab0d4ae9c0f24e7e051840
Abhinav <abhinavshaji16@gmail.com>
2024-04-10 16:49:01 +0530

    function2 [objects and array]

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,9 +19,43 @@ const result1 = addTwoNumbers(2, 3)
 
 // console.log(result1);
 
-function userLoginMessage(username) {
+function userLoginMessage(username = "Anil") {
   return `${username} just logged in`
 }
 
-console.log(userLoginMessage("Abhinav"));
+// console.log(userLoginMessage("Abhinav"));
 
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 500));
+
+
+const user = {
+  username: "Abhinav",
+  price: 199
+}
+
+
+function handleObject (anyobject) {
+  // console.log(`Username is ${anyobject.username} price is ${anyobject.price}`);
+}
+
+
+// handleObject(user)
+handleObject({
+  username:"Anil",
+  price:200
+})
+
+
+const myArray = [200, 400, 600, 700]
+
+function returnSecondValue(getArray) {
+  return getArray[1]
+}
+
+// console.log(returnSecondValue(myArray));
+console.log(returnSecondValue([1, 2, 3, 4, 5]));
