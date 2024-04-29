57bda3a06ff3f99cafa4416cfac070f875e29f14
Abhinav <abhinavshaji16@gmail.com>
2024-04-29 16:07:54 +0530

    special operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -11,3 +11,18 @@ if (userEmail) {
 
 //truthy
 // "0", "false", " ", [], {}, function() {}
+
+// ??
+
+a = null ?? 10
+b = undefined ?? 20
+
+console.log(`${a} ${b}`);
+
+
+// Terninary Operator
+// condition ? true : false
+
+const teaPrice = 60
+
+teaPrice > 50 ? console.log("price greater than 50") : console.log("price less than 50");
