1473a0053d4f0acbc080c3bd002b73b53e70acd8
Abhinav <abhinavshaji16@gmail.com>
2024-04-29 15:51:10 +0530

    truthy falsy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,13 @@
+const userEmail = []
+
+if (userEmail) {
+  console.log("Got user Email");
+} else {
+  console.log("Dont have user email");
+}
+
+//falsy
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy
+// "0", "false", " ", [], {}, function() {}
