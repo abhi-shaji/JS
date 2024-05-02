f6f4e7de48f6ddfbe6724a4226a48171d85f9c1e
Abhinav <abhinavshaji16@gmail.com>
2024-05-02 13:46:41 +0530

    for loop(break and continue)

---
+++ 05_iterations/one.js
@@ -0,0 +1,33 @@
+// for
+
+
+// for (let index = 0; index < 10; index++) {
+//   const element = index;
+//   console.log(element);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   // console.log(`Outer loop ${i}`);
+//   for (let j = 1; j <= 10; j++) {
+//     // console.log(`inner loop ${j}`);
+//     console.log(`${i} * ${j} == ${i * j}`);
+//   }
+  
+// }
+
+
+// for (let index = 0; index <= 20; index++) {
+// if (index == 5) {
+//   break
+// }
+//   console.log(`value is ${index}`);
+  
+// }
+
+for (let index = 0; index <= 20; index++) {
+if (index == 5) {
+  continue
+}
+  console.log(`value is ${index}`);
+  
+}
